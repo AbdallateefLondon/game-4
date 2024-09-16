3ff324660a5b36906e1e62412d87322610388ff0
AbdallateefLondon <abdallaabdallateef8@gmail.com>
2024-09-16 13:48:43 +0300

    Added new-folder with files

---
+++ new-folder/application/config/license.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+$config['envato_market_purchase_code'] = '';
+$config['envato_market_username'] = '';
+$config['SSLK'] = 'LEKGQC-GCH8HA-9YQEH8-di8xQ0Zrd0lPNGUzcUNPVVBBZDdFSWU1eDJUSkEza3RuemhXdExaeGpSTT0=';
+$config['app_ver'] = 0;
